5efec843d3f28155ae907560f4d430c2f9da429c
Michaelmvv <bukkit.michaelmvv@gmail.com>
2014-12-21 11:15:20 -0800

    Op Toggle

---
+++ OpToggle/src/michaelmvv/OpToggle/Toggle.java
@@ -0,0 +1,55 @@
+package michaelmvv.OpToggle;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Toggle extends JavaPlugin {
+	Logger log;
+
+	@Override
+	public void onEnable() {
+		log = Bukkit.getLogger();
+		log.info("Mvv OpToggle starting");
+		super.onEnable();
+	}
+
+	@Override
+	public void onDisable() {
+		log.info("Mvv opToggle shutting down");
+		super.onDisable();
+	}
+
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+
+		if (command.getName().equalsIgnoreCase("opToggle")) {
+			if (sender instanceof Player) {
+				if (sender.hasPermission("opme.admin")) {
+					if (sender.isOp()) {
+						sender.setOp(false);
+						sender.sendMessage(ChatColor.GREEN
+								+ "You are nolonger op");
+						log.info(sender.getName() + " is nolonger Op");
+					} else {
+						sender.setOp(true);
+						log.info(sender.getName() + " is now Op");
+						sender.sendMessage(ChatColor.GREEN + "You are now op");
+					}
+				} else {
+					sender.sendMessage(ChatColor.RED + "You cant do this...");
+				}
+			} else {
+				sender.sendMessage("Only players can use this command");
+			}
+		}
+
+		return super.onCommand(sender, command, label, args);
+	}
+
+}
