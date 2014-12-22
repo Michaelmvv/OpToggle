0ec33a74558c9fabcecd285d53967c4984a65489
Michaelmvv <bukkit.michaelmvv@gmail.com>
2014-12-21 20:45:32 -0800

    Add comments

--- OpToggle/src/opToggle/Toggle.java
+++ OpToggle/src/opToggle/Toggle.java
@@ -27,24 +27,32 @@ public class Toggle extends JavaPlugin {
 
 	public boolean onCommand(CommandSender sender, Command command,
 			String label, String[] args) {
-
+		// See if the command is opToggle
 		if (command.getName().equalsIgnoreCase("opToggle")) {
+			// see if a player sent it
 			if (sender instanceof Player) {
+				// check perms
 				if (sender.hasPermission("optoggle.toggle")) {
+					// check if player is Op
 					if (sender.isOp()) {
+						// if they are Op, de-op them
 						sender.setOp(false);
 						sender.sendMessage(ChatColor.GREEN
 								+ "You are nolonger op");
 						log.info(sender.getName() + " is nolonger Op");
 					} else {
+						// if player is not op, give them op
 						sender.setOp(true);
 						log.info(sender.getName() + " is now Op");
 						sender.sendMessage(ChatColor.GREEN + "You are now op");
 					}
 				} else {
-					sender.sendMessage(ChatColor.RED + "You don't have optoggle.toggle");
+					// tell player they don't have perms
+					sender.sendMessage(ChatColor.RED
+							+ "You don't have optoggle.toggle");
 				}
 			} else {
+				// tell non-player that they cant use the command
 				sender.sendMessage("Only players can use this command");
 			}
 		}
