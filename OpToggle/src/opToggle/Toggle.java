58b9230c7d3f480eecc940ca1384e30c267b011a
Michaelmvv <bukkit.michaelmvv@gmail.com>
2014-12-21 21:52:13 -0800

    Fix updater, Tell admins on login

--- OpToggle/src/opToggle/Toggle.java
+++ OpToggle/src/opToggle/Toggle.java
@@ -11,12 +11,15 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Toggle extends JavaPlugin {
+public class Toggle extends JavaPlugin implements Listener {
 	Logger log;
 	String ver = "1";
-	boolean hasUpdate;
+	boolean hasUpdate = false;
 
 	@Override
 	public void onEnable() {
@@ -24,13 +27,17 @@ public class Toggle extends JavaPlugin {
 		log = Bukkit.getLogger();
 		log.info("Mvv OpToggle starting");
 
+		// register the player join event thingy
+		getServer().getPluginManager().registerEvents(this, this);
+
+		// check if there are updates
 		ArrayList<String> information = new ArrayList<String>();
 		try {
 			URL remoteFile = new URL(
 					"https://raw.githubusercontent.com/Michaelmvv/OpToggle/master/Update/update.txt");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
 					remoteFile.openStream()));
-			for (int i = 0; i < 2; i++) {
+			for (int i = 0; i < 3; i++) {
 				information.add(reader.readLine());
 			}
 		} catch (Exception e) {
@@ -43,7 +50,9 @@ public class Toggle extends JavaPlugin {
 			log.info(ChatColor.RED + "OpToggle (Ver. " + ver
 					+ " ) is not in sync with github (Ver. "
 					+ information.get(0) + ")");
-			log.info("Get the update at: "+ChatColor.GREEN+"https://github.com/Michaelmvv/OpToggle");
+			log.info("Get the update at: " + ChatColor.GREEN
+					+ "https://github.com/Michaelmvv/OpToggle");
+			hasUpdate = true;
 		}
 
 	}
@@ -51,6 +60,26 @@ public class Toggle extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		log.info("Mvv opToggle shutting down");
+	}
+
+	//when player logs in
+	@EventHandler
+	public void onLogin(PlayerLoginEvent event) {
+		//get player from event
+		Player player = event.getPlayer();
+		//check if player is op
+		if (player.isOp()) {
+			//check for update 
+			if (hasUpdate) {
+				//tell OP player that there is an update
+				player.sendMessage(ChatColor.RED + "OpToggle Has an update");
+				player.sendMessage(ChatColor.RED + "Get it at:");
+				player.sendMessage(ChatColor.GREEN
+						+ "https://github.com/Michaelmvv/OpToggle");
+
+			}
+		}
+
 	}
 
 	public boolean onCommand(CommandSender sender, Command command,
